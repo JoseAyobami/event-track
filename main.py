7f36b107ee005f0fb6846d965b0b7e21eb28934f
Jose <ayenijoseph45@gmail.com>
2025-06-12 10:13:40 +0100

    made main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from routes.user import user_router
+from routes.event import event_router
+
+
+
+app = FastAPI()
+app.include_router(user_router, prefix="/users", tags=["users"])
+app.include_router(event_router, prefix="/events", tags=["events"])
+
+
+
