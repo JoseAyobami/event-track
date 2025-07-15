50d0ffb9d457734551791a74bb50fd671cfb6134
Jose <ayenijoseph45@gmail.com>
2025-07-15 16:04:38 +0100

    modified register routes

--- routes/register.py
+++ routes/register.py
@@ -13,14 +13,17 @@ def get_all_registrations():
     return registration_service.get_all_registrations()
 
 @registration_router.get("/{user_id}/", status_code=200)
-def get_user_registrations(user_id: int):
-    return registration_service.get_user_registrations(user_id)
+def get_user_registrations(user_id: int = None):
+    if user_id:
+        return registration_service.get_user_registrations(user_id)
+    return registration_service.get_all_registrations()
+
 
 @registration_router.get("/{event_id}/registration", status_code=200)
 def get_event_registration(event_id: int):
     return registration_service.get_event_registration(event_id)
 
-@registration_router.post("/{event_id}/attendance", status_code=200)
-def mark_attendance(event_id: int, user_id: int):
-    return registration_service.mark_attendance(event_id, user_id)
+@registration_router.patch("/{registration_id}/attendance", status_code=200)
+def mark_attendance(registration_id: int):
+    return registration_service.mark_attendance(registration_id)
 
